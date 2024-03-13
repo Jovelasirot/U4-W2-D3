d19ff15f11e4508b4bb175ca8b300f8641922b26
Jovelasirot <jovelasirot@gmail.com>
2024-03-13 15:29:44 +0100

    Es3

--- src/es3/Es3.java
+++ src/es3/Es3.java
@@ -1,5 +1,49 @@
 package es3;
 
-public class Es3 {
+import entities.Product;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class Es3 {
+    public static void main(String[] args) {
+        Random rdm = new Random();
+
+        Supplier<Double> rdmPrice = () -> {
+            return rdm.nextDouble(101, 200);
+        };
+
+        Supplier<Long> rdmId = () -> {
+            return rdm.nextLong(1, 100);
+        };
+
+        Supplier<Product> productSupplier1 = () -> new Product(rdmId.get(), "Random Boys product", "Tea", rdmPrice.get());
+
+        Supplier<Product> productSupplier = () -> new Product(rdmId.get(), "Random Boys product", "Boys", rdmPrice.get());
+
+        List<Product> productList = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            productList.add(productSupplier.get());
+            productList.add(productSupplier1.get());
+        }
+
+
+        System.out.println("All products");
+        System.out.println(productList);
+        System.out.println(productSupplier1);
+
+        System.out.println("-------------------------------");
+
+
+        List<Product> boysProducts = productList.stream().filter(product -> product.getCategory().equals("Boys")).map(product -> {
+            double discountedPrice = product.getPrice() - (product.getPrice() / 10);
+            return new Product(product.getId(), product.getName(), product.getCategory(), discountedPrice);
+        }).toList();
+
+
+        System.out.println("Discounted boys products");
+        System.out.println(boysProducts);
+    }
 }
